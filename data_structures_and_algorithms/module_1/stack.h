e819920022aad1839c40d296468ef347252576b6
Flinnj77AF <flinn_james@ymail.com>
2024-06-28 13:16:34 -0600

    add data_structures and aglorithems

---
+++ data_structures_and_algorithms/module_1/stack.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+
+class stack
+{
+    vector<int> data;
+    int top;
+
+private:
+    /* data */
+public:
+    stack(/* args */);
+    ~stack();
+};
